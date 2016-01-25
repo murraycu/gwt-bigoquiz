70efd77f24618180875c351b63cd0883525966c9
Murray Cumming <murrayc@murrayc.com>
2016-01-25 09:59:45 +0100

    0.3

--- src/main/java/com/murrayc/bigoquiz/client/application/about/AboutView.java
+++ src/main/java/com/murrayc/bigoquiz/client/application/about/AboutView.java
@@ -30,7 +30,7 @@ public class AboutView extends ViewWithUiHandlers<AboutUserEditUiHandlers>
 
         //TODO: Internationalization:
         //TODO: Get the number from pom.xml somehow.
-        versionLabel.setText("Version: " + "0.2");
+        versionLabel.setText("Version: " + "0.3");
 
         initWidget(mainPanel);
     }
