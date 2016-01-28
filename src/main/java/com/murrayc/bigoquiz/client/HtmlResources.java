6c9923ccf50d92083229a42d86baf52e4c51a395
Murray Cumming <murrayc@murrayc.com>
2016-01-28 09:46:35 +0100

    Move the Recommended Reading HTML into a resource.

---
+++ src/main/java/com/murrayc/bigoquiz/client/HtmlResources.java
@@ -0,0 +1,15 @@
+package com.murrayc.bigoquiz.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+/**
+ * Created by murrayc on 1/28/16.
+ */
+public interface HtmlResources extends ClientBundle {
+    public static final HtmlResources INSTANCE = GWT.create(HtmlResources.class);
+
+    @Source("com/murrayc/bigoquiz/client/ui/reading.html")
+    public TextResource getReadingHtml();
+}
--- src/main/java/com/murrayc/bigoquiz/client/application/ApplicationView.java
+++ src/main/java/com/murrayc/bigoquiz/client/application/ApplicationView.java
@@ -1,9 +1,11 @@
 package com.murrayc.bigoquiz.client.application;
 
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.gwtplatform.mvp.client.ViewImpl;
+import com.murrayc.bigoquiz.client.HtmlResources;
 
 /**
  * Created by murrayc on 1/21/16.
@@ -11,12 +13,23 @@ import com.gwtplatform.mvp.client.ViewImpl;
 public class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
     private final Panel main = new FlowPanel();
     private final SimplePanel mainPanel = new SimplePanel();
+    private final SimplePanel readingPanel = new SimplePanel();
     private final SimplePanel menuPanel = new SimplePanel();
     private final SimplePanel userStatusPanel = new SimplePanel();
     private final SimplePanel userHistoryRecentPanel = new SimplePanel();
 
     ApplicationView() {
         main.add(menuPanel);
+
+        //Add some static HTML:
+        //TODO: It would be nicer to have this right in the main .html file,
+        //but then we couldn't put it in the correct <div>.
+        final HTML htmlPanel = new HTML();
+        final String html = HtmlResources.INSTANCE.getReadingHtml().getText();
+        htmlPanel.setHTML(html);
+        readingPanel.add(htmlPanel);
+        main.add(readingPanel);
+
         main.add(userStatusPanel);
 
         //We use a CSS media query to only show this on wider screens:
