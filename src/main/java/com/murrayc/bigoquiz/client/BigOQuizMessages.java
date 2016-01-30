2fc146905ae6c2dceb657325340daa4a460affbf
Murray Cumming <murrayc@murrayc.com>
2016-01-30 23:59:08 +0100

    Make translatable plurals/singular work.

--- src/main/java/com/murrayc/bigoquiz/client/BigOQuizMessages.java
+++ src/main/java/com/murrayc/bigoquiz/client/BigOQuizMessages.java
@@ -3,10 +3,13 @@ package com.murrayc.bigoquiz.client;
 //This is not generated with the 18nCreator (for instance, via the maven-gwt-plugin's gwt:i18n goal
 //using <i18nMessagesBundles> declarations.
 
+import com.google.gwt.i18n.client.LocalizableResource;
+
 /**
  * Interface to represent the messages contained in resource bundle:
  * /home/murrayc/checkout/github/gwt-bigoquiz/src/main/resources/com/murrayc/bigoquiz/client/ui/BigOQuizMessages.properties'.
  */
+@LocalizableResource.DefaultLocale("en")
 public interface BigOQuizMessages extends com.google.gwt.i18n.client.Messages {
 
     /**
@@ -15,8 +18,9 @@ public interface BigOQuizMessages extends com.google.gwt.i18n.client.Messages {
      * @return translated "Answered: {0,number}: , Correct: {1,number}"
      */
     @DefaultMessage("{1,number} correct / {0,number} answers")
+    @AlternateMessage({"one", "{1,number} correct / {0,number} answer"})
     @Key("scoreMessage")
-    String scoreMessage(int arg0, int arg1);
+    String scoreMessage(@PluralCount int arg0, int arg1);
 
     @DefaultMessage("{0,number} questions")
     @AlternateMessage({"one", "1 question"})
