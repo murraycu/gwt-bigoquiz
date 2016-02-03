a9dded556f904b06f2583f5cdf72227d2a4d3265
Murray Cumming <murrayc@murrayc.com>
2016-02-03 13:57:50 +0100

    Remove unused Constants.

--- src/main/java/com/murrayc/bigoquiz/shared/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.murrayc.bigoquiz.shared;
-
-/**
- * Created by murrayc on 1/26/16.
- */
-public class Constants {
-    //How many items to return.
-    public static final int HISTORY_LIMIT = 5;
-}
