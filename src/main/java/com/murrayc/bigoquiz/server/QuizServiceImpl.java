3828f1048057c810c09e4741f14bba49116825b4
Murray Cumming <murrayc@murrayc.com>
2016-02-04 12:19:11 +0100

    QuizServiceImpl: Reuse a query for UserStats.
    Instead of getting it one more time, just to write the changes back.
    We can do this because we always want to get the next question to send back along
    with the result/answer for the current question.

--- src/main/java/com/murrayc/bigoquiz/server/QuizServiceImpl.java
+++ src/main/java/com/murrayc/bigoquiz/server/QuizServiceImpl.java
@@ -76,6 +76,8 @@ public class QuizServiceImpl extends ServiceWithUser implements
 
     @Override
     public Question getNextQuestion(final String sectionId) throws IllegalArgumentException {
+        final Quiz quiz = getQuiz();
+
         final String userId = getUserId();
         if (StringUtils.isEmpty(userId)) {
             //The user is not logged in,
@@ -83,51 +85,15 @@ public class QuizServiceImpl extends ServiceWithUser implements
             return quiz.getRandomQuestion(sectionId);
         }
 
-        final Quiz quiz = getQuiz();
-
-        Map<String, UserStats> mapUserStats = getUserStats(userId);
-        final int MAX_TRIES = 10;
-        int tries = 0;
-        Question question = null;
-        Question questionBestSoFar = null;
-        int questionBestCountAnsweredWrong = 0;
-        while(tries < MAX_TRIES) {
-            tries += 1;
-
-            question = quiz.getRandomQuestion(sectionId);
-            if (question == null) {
-                continue;
-            }
-
-            if (questionBestSoFar == null) {
-                questionBestSoFar = question;
-            }
-
-            final UserStats userStats = mapUserStats.get(question.getSectionId());
-            if (userStats == null) {
-                //Assume this means the user has never answered any question in the section.
-                return question;
-            }
-
-            final String questionId = question.getId();
-
-            //Prioritize questions that have never been asked.
-            if (!userStats.getQuestionWasAnswered(questionId)) {
-                return question;
-            }
-
-            //Otherwise, try a few times to get a question that
-            //we have got wrong many times:
-            //We could just get the most-wrong answer directly,
-            //but we want some randomness.
-            final int countAnsweredWrong = userStats.getQuestionCountAnsweredWrong(questionId);
-            if (countAnsweredWrong > questionBestCountAnsweredWrong) {
-                questionBestSoFar = question;
-                questionBestCountAnsweredWrong = countAnsweredWrong;
-            }
+        if (StringUtils.isEmpty(sectionId)) {
+            final Map<String, UserStats> mapUserStats = getUserStats(userId);
+            return getNextQuestionFromUserStats(sectionId, quiz, mapUserStats);
+        } else {
+            //This special case is a bit copy-and-pasty of the general case with the
+            //map, but it seems more efficient to avoid an unncessary Map.
+            final UserStats userStats = getUserStatsForSection(userId, sectionId);
+            return getNextQuestionFromUserStatsForSection(sectionId, quiz, userStats);
         }
-
-        return questionBestSoFar;
     }
 
     @Override
@@ -143,16 +109,19 @@ public class QuizServiceImpl extends ServiceWithUser implements
             throw new IllegalArgumentException("Unknown QuestionAndAnswer ID");
         }
 
-        //Store whether we got the question right or wrong:
         final boolean result = StringUtils.equals(questionAndAnswer.getAnswer(), answer);
-        storeAnswer(result, questionAndAnswer.getQuestion());
-
-        return createSubmissionResult(result, questionId, nextQuestionSectionId);
+        return storeAnswerCorrectnessAndGetSubmissionResult(questionId, nextQuestionSectionId, questionAndAnswer, result);
     }
 
     @Override
     public SubmissionResult submitDontKnowAnswer(final String questionId, final String nextQuestionSectionId) throws IllegalArgumentException {
-        return createSubmissionResult(false, questionId, nextQuestionSectionId);
+        final QuestionAndAnswer questionAndAnswer = getQuestionAndAnswer(questionId);
+        if (questionAndAnswer == null) {
+            throw new IllegalArgumentException("Unknown QuestionAndAnswer ID");
+        }
+
+        //Store this like a don't know answer:
+        return storeAnswerCorrectnessAndGetSubmissionResult(questionId, nextQuestionSectionId, questionAndAnswer, false);
     }
 
     private QuestionAndAnswer getQuestionAndAnswer(final String questionId) {
@@ -169,7 +138,8 @@ public class QuizServiceImpl extends ServiceWithUser implements
     public UserRecentHistory getUserRecentHistory() throws IllegalArgumentException {
         final String userId = getUserId();
         if (StringUtils.isEmpty(userId)) {
-            Log.error("getUserRecentHistory(): userId was null.");
+            //This is normal, if the user is not logged in.
+            //Log.error("getUserRecentHistory(): userId was null.");
             return null;
         }
 
@@ -329,26 +299,50 @@ public class QuizServiceImpl extends ServiceWithUser implements
         return quiz;
     }
 
-    private SubmissionResult createSubmissionResult(boolean result, final String questionId, final String nextQuestionSectionId) {
+    private SubmissionResult createSubmissionResult(boolean result, final String questionId, final String nextQuestionSectionId, final Map<String, UserStats> mapUserStats) {
+        final Quiz quiz = getQuiz();
+
         //We only provide the correct answer if the supplied answer was wrong:
         String correctAnswer = null;
         if (!result) {
-            final Quiz quiz = getQuiz();
             correctAnswer = quiz.getAnswer(questionId);
         }
 
-        final Question nextQuestion = getNextQuestion(nextQuestionSectionId);
+        final Question nextQuestion = getNextQuestionFromUserStats(nextQuestionSectionId, quiz, mapUserStats);
         return new SubmissionResult(result, correctAnswer, nextQuestion);
     }
 
-    private void storeAnswer(boolean result, final Question question) {
-        final String userId = getUserId();
+    /**
+     *
+     * @param result
+     * @param questionId
+     * @param nextQuestionSectionId
+     * @param userStats This may be null if nextQuestionSectionId is null.
+     * @return
+     */
+    private SubmissionResult createSubmissionResultForSection(boolean result, final String questionId, final String nextQuestionSectionId, final UserStats userStats) {
+        final Quiz quiz = getQuiz();
+
+        //We only provide the correct answer if the supplied answer was wrong:
+        String correctAnswer = null;
+        if (!result) {
+            correctAnswer = quiz.getAnswer(questionId);
+        }
+
+        final Question nextQuestion = getNextQuestionFromUserStatsForSection(nextQuestionSectionId, quiz, userStats);
+        return new SubmissionResult(result, correctAnswer, nextQuestion);
+    }
+
+    private void storeAnswer(boolean result, final Question question, final String userId, final Map<String, UserStats> mapUserStats) {
         if (StringUtils.isEmpty(userId)) {
             Log.error("storeAnswer(): userId was null.");
             return;
         }
 
-        final EntityManagerFactory emf = EntityManagerFactory.get();
+        if (mapUserStats == null) {
+            Log.error("storeAnswer(): mapUserStats was null.");
+            return;
+        }
 
         //Update the statistics:
         final String sectionId = question.getSectionId();
@@ -357,8 +351,34 @@ public class QuizServiceImpl extends ServiceWithUser implements
             return;
         }
 
-        UserStats userStats = getUserStatsForSection(userId, sectionId);
+        UserStats userStats = mapUserStats.get(sectionId);
         if (userStats == null) {
+            userStats = new UserStats(userId, sectionId);
+        }
+
+        storeAnswerForSection(result, question, userId, userStats);
+    }
+
+    private void storeAnswerForSection(boolean result, final Question question, final String userId, UserStats userStats) {
+        if (question == null) {
+            Log.error("storeAnswerForSection(): question is null.");
+            return;
+        }
+
+        //Create new UserStats if necessary,
+        //for instance if this is the first time storing an answer for this section.
+        if (userStats == null) {
+            if (StringUtils.isEmpty(userId)) {
+                Log.error("storeAnswerForSection(): userId is empty.");
+                return;
+            }
+
+            final String sectionId = question.getSectionId();
+            if (StringUtils.isEmpty(sectionId)) {
+                Log.error("storeAnswerForSection(): sectionId is empty.");
+                return;
+            }
+
             userStats = new UserStats(userId, sectionId);
         }
 
@@ -370,6 +390,7 @@ public class QuizServiceImpl extends ServiceWithUser implements
 
         userStats.updateProblemQuestion(question, result);
 
+        final EntityManagerFactory emf = EntityManagerFactory.get();
         emf.ofy().save().entity(userStats).now();
     }
 
@@ -383,10 +404,15 @@ public class QuizServiceImpl extends ServiceWithUser implements
     }
     */
 
+    /**
+     *
+     * @return null if the user is not logged in.
+     */
     private String getUserId() {
         final UserProfile userProfile = getUserProfile();
         if (userProfile == null) {
-            Log.error("getUserId(): userProfile was null.");
+            //This is normal if the user is not logged in.
+            //Log.error("getUserId(): userProfile was null.");
             return null;
         }
 
@@ -396,5 +422,108 @@ public class QuizServiceImpl extends ServiceWithUser implements
         }
 
         return userId;
+    }
+
+    private SubmissionResult storeAnswerCorrectnessAndGetSubmissionResult(final String questionId, final String nextQuestionSectionId, final QuestionAndAnswer questionAndAnswer, boolean result) {
+        //If the user is logged in, store whether we got the question right or wrong:
+        final String userId = getUserId();
+
+        final String sectionId = questionAndAnswer.getQuestion().getSectionId();
+
+        // Get the UserStats (or a map of them), and use it for both storing the answer and getting the next question,
+        // to avoid getting the UserStats twice from the datastore.
+        //
+        // Call different methods depending on whether nextQuestionSectionId is specified an as is the same as the
+        // questino's section ID, to avoid allocating a Map just containing one UserStats.
+        if (!StringUtils.isEmpty(nextQuestionSectionId) &&
+                StringUtils.equals(nextQuestionSectionId, sectionId)) {
+            UserStats userStats = null;
+            if (!StringUtils.isEmpty(userId)) {
+                userStats = getUserStatsForSection(userId, nextQuestionSectionId);
+                storeAnswerForSection(result, questionAndAnswer.getQuestion(), userId, userStats);
+            }
+
+            return createSubmissionResultForSection(result, questionId, nextQuestionSectionId, userStats);
+        } else {
+            Map<String, UserStats> mapUserStats = null;
+            if (!StringUtils.isEmpty(userId)) {
+                mapUserStats = getUserStats(userId);
+                storeAnswer(result, questionAndAnswer.getQuestion(), userId, mapUserStats);
+            }
+
+            return createSubmissionResult(result, questionId, nextQuestionSectionId, mapUserStats);
+        }
+    }
+
+    /**
+     *
+     * @param sectionId
+     * @param quiz
+     * @param userStats Can be null.
+     * @return
+     */
+    private Question getNextQuestionFromUserStatsForSection(final String sectionId, final Quiz quiz, final UserStats userStats) {
+        if (StringUtils.isEmpty(sectionId)) {
+            Log.error("getNextQuestionFromPerSectionUserStat(): sectionId was null.");
+            return null;
+        }
+
+        //TODO: Avoid this temporary map:
+        final Map<String, UserStats> map = new HashMap<>();
+        if (userStats != null) {
+            map.put(userStats.getSectionId(), userStats);
+        }
+
+        return getNextQuestionFromUserStats(sectionId, quiz, map);
+    }
+
+    private Question getNextQuestionFromUserStats(final String sectionId, final Quiz quiz, final Map<String, UserStats> mapUserStats) {
+        final int MAX_TRIES = 10;
+        int tries = 0;
+        Question question = null;
+        Question questionBestSoFar = null;
+        int questionBestCountAnsweredWrong = 0;
+        while(tries < MAX_TRIES) {
+            tries += 1;
+
+            question = quiz.getRandomQuestion(sectionId);
+            if (question == null) {
+                continue;
+            }
+
+            if (questionBestSoFar == null) {
+                questionBestSoFar = question;
+            }
+
+            if (mapUserStats == null) {
+                //Assume this means the user has never answered any question in any section.
+                return question;
+            }
+
+            final UserStats userStats = mapUserStats.get(question.getSectionId());
+            if (userStats == null) {
+                //Assume this means the user has never answered any question in the section.
+                return question;
+            }
+
+            final String questionId = question.getId();
+
+            //Prioritize questions that have never been asked.
+            if (!userStats.getQuestionWasAnswered(questionId)) {
+                return question;
+            }
+
+            //Otherwise, try a few times to get a question that
+            //we have got wrong many times:
+            //We could just get the most-wrong answer directly,
+            //but we want some randomness.
+            final int countAnsweredWrong = userStats.getQuestionCountAnsweredWrong(questionId);
+            if (countAnsweredWrong > questionBestCountAnsweredWrong) {
+                questionBestSoFar = question;
+                questionBestCountAnsweredWrong = countAnsweredWrong;
+            }
+        }
+
+        return questionBestSoFar;
     }
 }
