6eb2ef24da1ca1fb0ce999870e1d5316c2ec100f
Murray Cumming <murrayc@murrayc.com>
2016-02-23 14:03:18 +0100

    HomeView: Make sure there is a content-panel.
    So it can have smaller vertical margins on small devices.

--- src/main/java/com/murrayc/bigoquiz/client/application/home/HomeView.java
+++ src/main/java/com/murrayc/bigoquiz/client/application/home/HomeView.java
@@ -3,6 +3,7 @@ package com.murrayc.bigoquiz.client.application.home;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.murrayc.bigoquiz.client.HtmlResources;
 import com.murrayc.bigoquiz.client.application.ContentViewWithUIHandlers;
 import com.murrayc.bigoquiz.client.application.Utils;
@@ -15,7 +16,10 @@ public class HomeView extends ContentViewWithUIHandlers<HomeUserEditUiHandlers>
         implements HomePresenter.MyView {
 
     HomeView() {
-        Utils.addHtmlToPanel(mainPanel, HtmlResources.INSTANCE.getHomeHtml());
+        //If we just add the HTML directly to mainPanel, we will lose its CSS id.
+        final FlowPanel panel = new FlowPanel();
+        Utils.addHtmlToPanel(panel, HtmlResources.INSTANCE.getHomeHtml());
+        mainPanel.add(panel);
 
         final Button buttonPlay = new Button(constants.buttonAnswerQuestions());
         buttonPlay.addStyleName("home-button-answer-questions");
