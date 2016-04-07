70f89eeeff0283ba5fa5b0126314311eaa421b28
Murray Cumming <murrayc@murrayc.com>
2016-04-07 12:34:09 +0200

    Remove unnecessary import.

--- src/main/java/com/murrayc/bigoquiz/client/application/home/HomeView.java
+++ src/main/java/com/murrayc/bigoquiz/client/application/home/HomeView.java
@@ -1,6 +1,5 @@
 package com.murrayc.bigoquiz.client.application.home;
 
-import com.google.gwt.dom.client.ParagraphElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
