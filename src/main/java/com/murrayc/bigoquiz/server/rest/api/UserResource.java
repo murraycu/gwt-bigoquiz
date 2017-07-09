c0a133690fe4b1bf0f01a7993c1268b3fe581786
Murray Cumming <murrayc@murrayc.com>
2017-07-09 23:10:20 +0200

    Rest: Add /api/user URL.

---
+++ src/main/java/com/murrayc/bigoquiz/server/rest/api/UserResource.java
@@ -0,0 +1,34 @@
+package com.murrayc.bigoquiz.server.rest.api;
+
+import com.murrayc.bigoquiz.client.LoginInfo;
+import com.murrayc.bigoquiz.server.QuizzesMap;
+import com.murrayc.bigoquiz.server.ServiceUserUtils;
+import org.fusesource.restygwt.client.RestService;
+
+import javax.servlet.http.HttpServlet;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+/**
+ * Created by murrayc on 7/7/17.
+ */
+@Path("user")
+public class UserResource {
+    public UserResource() {
+    }
+
+    /**
+     * requestUrl is the URL that the user should be returned to after logging in or out.
+     * It corresponds to the destinationURL parameter to UserService.createLogoutURL():
+     * https://cloud.google.com/appengine/docs/standard/java/javadoc/com/google/appengine/api/users/UserService.html#createLogoutURL-java.lang.String-java.lang.String-
+     *
+     * @return
+     */
+    @GET
+    @Produces("application/json")
+    public LoginInfo get(@QueryParam("requestUrl") String requestUrl) {
+        return ServiceUserUtils.getLoginInfo(requestUrl);
+    }
+}
