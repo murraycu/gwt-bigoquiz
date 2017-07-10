469e20445b84b07ff5bd3cb173d4890587c8af31
Murray Cumming <murrayc@murrayc.com>
2017-07-10 08:41:53 +0200

    Rest: UserHistoryResource: Add /api/submit-answer and /api/submit-dont-know-answer POSTs.

--- src/main/java/com/murrayc/bigoquiz/server/rest/api/UserHistoryResource.java
+++ src/main/java/com/murrayc/bigoquiz/server/rest/api/UserHistoryResource.java
@@ -3,15 +3,13 @@ package com.murrayc.bigoquiz.server.rest.api;
 import com.google.appengine.api.users.User;
 import com.googlecode.objectify.cmd.Query;
 import com.murrayc.bigoquiz.client.*;
+import com.murrayc.bigoquiz.server.QuizUtils;
 import com.murrayc.bigoquiz.server.ServiceUserUtils;
 import com.murrayc.bigoquiz.server.db.EntityManagerFactory;
-import com.murrayc.bigoquiz.shared.Question;
-import com.murrayc.bigoquiz.shared.Quiz;
-import com.murrayc.bigoquiz.shared.QuizSections;
+import com.murrayc.bigoquiz.shared.*;
 import com.murrayc.bigoquiz.shared.db.UserQuestionHistory;
 import com.murrayc.bigoquiz.shared.db.UserStats;
 import org.apache.commons.lang3.StringUtils;
-import org.fusesource.restygwt.client.RestService;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -187,6 +185,278 @@ public class UserHistoryResource extends ResourceWithQuizzes {
             //TODO: Batch these:
             EntityManagerFactory.ofy().delete().entity(userStats).now();
         }
+    }
+
+    /**
+     * submitAnswer() returns the correct correctAnswer (if the supplied correctAnswer was wrong) and the next question.
+     * This avoids the client needing to make multiple calls.
+     *
+     * @param questionId
+     * @param answer
+     * @param nextQuestionSectionId
+     * @return
+     * @throws IllegalArgumentException
+     */
+    @POST
+    @Path("/submit-answer")
+    @Produces("application/json")
+    @NotNull
+    public SubmissionResult submitAnswer(@QueryParam("quizId") final String quizId, @QueryParam("questionId") final String questionId, @QueryParam("answer") final String answer, @QueryParam("nextQuestionSectionId") final String nextQuestionSectionId) throws IllegalArgumentException {
+        @Nullable final QuestionAndAnswer questionAndAnswer = getQuestionAndAnswer(quizId, questionId);
+        if (questionAndAnswer == null) {
+            throw new IllegalArgumentException("Unknown QuestionAndAnswer ID");
+        }
+
+        final Question.Text correctAnswer = questionAndAnswer.getAnswer();
+        if (correctAnswer == null) {
+            throw new RuntimeException("submitAnswer(): correctAnswer was null.");
+        }
+
+        final boolean result = QuizUtils.answerIsCorrect(answer, correctAnswer.text);
+
+        return storeAnswerCorrectnessAndGetSubmissionResult(quizId, questionId, nextQuestionSectionId, questionAndAnswer, result);
+    }
+
+    @POST
+    @Path("/submit-dont-know-answer")
+    @Produces("application/json")
+    @NotNull
+    public SubmissionResult submitDontKnowAnswer(@QueryParam("quizId") final String quizId, @QueryParam("questionId") final String questionId, @QueryParam("nextQuestionSectionId") final String nextQuestionSectionId) throws IllegalArgumentException {
+        @Nullable final QuestionAndAnswer questionAndAnswer = getQuestionAndAnswer(quizId, questionId);
+        if (questionAndAnswer == null) {
+            throw new IllegalArgumentException("Unknown QuestionAndAnswer ID");
+        }
+
+        //Store this like a don't know answer:
+        return storeAnswerCorrectnessAndGetSubmissionResult(quizId, questionId, nextQuestionSectionId, questionAndAnswer, false);
+    }
+
+
+    @NotNull
+    private SubmissionResult storeAnswerCorrectnessAndGetSubmissionResult(final String quizId, final String questionId, final String nextQuestionSectionId, @NotNull final QuestionAndAnswer questionAndAnswer, boolean result) {
+        //If the user is logged in, store whether we got the question right or wrong:
+        @Nullable final String userId = getUserId();
+
+        final String sectionId = questionAndAnswer.getQuestion().getSectionId();
+
+        // Get the UserStats (or a map of them), and use it for both storing the answer and getting the next question,
+        // to avoid getting the UserStats twice from the datastore.
+        //
+        // Call different methods depending on whether nextQuestionSectionId is specified an as is the same as the
+        // questino's section ID, to avoid allocating a Map just containing one UserStats.
+        if (!StringUtils.isEmpty(nextQuestionSectionId) &&
+                StringUtils.equals(nextQuestionSectionId, sectionId)) {
+            @Nullable UserStats userStats = null;
+            if (!StringUtils.isEmpty(userId)) {
+                userStats = getUserStatsForSection(userId, quizId, nextQuestionSectionId);
+                storeAnswerForSection(result, quizId, questionAndAnswer.getQuestion(), userId, userStats);
+            }
+
+            return createSubmissionResultForSection(result, quizId, questionId, nextQuestionSectionId, userStats);
+        } else {
+            @Nullable Map<String, UserStats> mapUserStats = null;
+            if (!StringUtils.isEmpty(userId)) {
+                mapUserStats = getUserStats(userId, quizId);
+                storeAnswer(result, quizId, questionAndAnswer.getQuestion(), userId, mapUserStats);
+            }
+
+            return createSubmissionResult(result, quizId, questionId, nextQuestionSectionId, mapUserStats);
+        }
+    }
+
+    private UserStats getUserStatsForSection(@NotNull final String userId, @NotNull final String quizId, @NotNull final String sectionId) {
+        Query<UserStats> q = getQueryForUserStats(userId, quizId);
+        q = q.filter("sectionId", sectionId);
+        q = q.limit(1);
+        final List<UserStats> list = q.list();
+        if (!list.isEmpty()) {
+            return list.get(0);
+        }
+
+        return null;
+    }
+
+
+    @NotNull
+    private SubmissionResult createSubmissionResult(boolean result, final String quizId, @NotNull final String questionId, @Nullable final String nextQuestionSectionId, @Nullable final Map<String, UserStats> mapUserStats) {
+        @NotNull final Quiz quiz = getQuiz(quizId);
+
+        //We only provide the correct answer if the supplied answer was wrong:
+        @Nullable Question.Text correctAnswer = null;
+        if (!result) {
+            correctAnswer = quiz.getAnswer(questionId);
+        }
+
+        @Nullable final Question nextQuestion = getNextQuestionFromUserStats(nextQuestionSectionId, quiz, mapUserStats);
+        return generateSubmissionResult(result, quiz, correctAnswer, nextQuestion);
+    }
+
+    @NotNull
+    private SubmissionResult generateSubmissionResult(boolean result, Quiz quiz, Question.Text correctAnswer, Question nextQuestion) {
+        setQuestionExtras(nextQuestion, quiz);
+        return new SubmissionResult(result, correctAnswer, nextQuestion);
+    }
+
+    /**
+     *
+     * @param result
+     * @param questionId
+     * @param nextQuestionSectionId
+     * @param userStats This may be null if nextQuestionSectionId is null.
+     * @return
+     */
+    @NotNull
+    private SubmissionResult createSubmissionResultForSection(boolean result, final String quizId, final String questionId, final String nextQuestionSectionId, final UserStats userStats) {
+        @NotNull final Quiz quiz = getQuiz(quizId);
+
+        //We only provide the correct answer if the supplied answer was wrong:
+        @Nullable Question.Text correctAnswer = null;
+        if (!result) {
+            correctAnswer = quiz.getAnswer(questionId);
+        }
+
+        @Nullable final Question nextQuestion = getNextQuestionFromUserStatsForSection(nextQuestionSectionId, quiz, userStats);
+        return generateSubmissionResult(result, quiz, correctAnswer, nextQuestion);
+    }
+
+    private void storeAnswer(boolean result, @NotNull final String quizId, @NotNull final Question question, final String userId, @Nullable final Map<String, UserStats> mapUserStats) {
+        if (StringUtils.isEmpty(userId)) {
+            Log.error("storeAnswer(): userId was null.");
+            return;
+        }
+
+        if (mapUserStats == null) {
+            Log.error("storeAnswer(): mapUserStats was null.");
+            return;
+        }
+
+        //Update the statistics:
+        final String sectionId = question.getSectionId();
+        if (StringUtils.isEmpty(sectionId)) {
+            Log.error("storeAnswer(): sectionId is null.");
+            return;
+        }
+
+        UserStats userStats = mapUserStats.get(sectionId);
+        if (userStats == null) {
+            userStats = new UserStats(userId, quizId, sectionId);
+        }
+
+        storeAnswerForSection(result, quizId, question, userId, userStats);
+    }
+
+    private void storeAnswerForSection(boolean result, @NotNull final String quizId, @Nullable final Question question, final String userId, @Nullable UserStats userStats) {
+        if (question == null) {
+            Log.error("storeAnswerForSection(): question is null.");
+            return;
+        }
+
+        //Create new UserStats if necessary,
+        //for instance if this is the first time storing an answer for this section.
+        if (userStats == null) {
+            if (StringUtils.isEmpty(userId)) {
+                Log.error("storeAnswerForSection(): userId is empty.");
+                return;
+            }
+
+            final String sectionId = question.getSectionId();
+            if (StringUtils.isEmpty(sectionId)) {
+                Log.error("storeAnswerForSection(): sectionId is empty.");
+                return;
+            }
+
+            userStats = new UserStats(userId, quizId, sectionId);
+        }
+
+        userStats.incrementAnswered();
+
+        if (result) {
+            userStats.incrementCorrect();
+        }
+
+        userStats.updateProblemQuestion(question, result);
+
+        EntityManagerFactory.ofy().save().entity(userStats).now();
+    }
+
+
+    /**
+     *
+     * @param sectionId
+     * @param quiz
+     * @param userStats Can be null.
+     * @return
+     */
+    private Question getNextQuestionFromUserStatsForSection(@NotNull final String sectionId, @NotNull final Quiz quiz, @Nullable final UserStats userStats) {
+        if (StringUtils.isEmpty(sectionId)) {
+            Log.error("getNextQuestionFromPerSectionUserStat(): sectionId was null.");
+            return null;
+        }
+
+        //TODO: Avoid this temporary map:
+        @NotNull final Map<String, UserStats> map = new HashMap<>();
+        if (userStats != null) {
+            map.put(userStats.getSectionId(), userStats);
+        }
+
+        return getNextQuestionFromUserStats(sectionId, quiz, map);
+    }
+
+    @Nullable
+    private Question getNextQuestionFromUserStats(@Nullable final String sectionId, @NotNull final Quiz quiz, @Nullable final Map<String, UserStats> mapUserStats) {
+        final int MAX_TRIES = 10;
+        int tries = 0;
+        @Nullable Question question = null;
+        @Nullable Question questionBestSoFar = null;
+        int questionBestCountAnsweredWrong = 0;
+        while(tries < MAX_TRIES) {
+            tries += 1;
+
+            question = quiz.getRandomQuestion(sectionId);
+            if (question == null) {
+                continue;
+            }
+
+            if (questionBestSoFar == null) {
+                questionBestSoFar = question;
+            }
+
+            if (mapUserStats == null) {
+                //Assume this means the user has never answered any question in any section.
+                return question;
+            }
+
+            final UserStats userStats = mapUserStats.get(question.getSectionId());
+            if (userStats == null) {
+                //Assume this means the user has never answered any question in the section.
+                return question;
+            }
+
+            final String questionId = question.getId();
+
+            //Prioritize questions that have never been asked.
+            if (!userStats.getQuestionWasAnswered(questionId)) {
+                return question;
+            }
+
+            //Otherwise, try a few times to get a question that
+            //we have got wrong many times:
+            //We could just get the most-wrong answer directly,
+            //but we want some randomness.
+            final int countAnsweredWrong = userStats.getQuestionCountAnsweredWrong(questionId);
+            if (countAnsweredWrong > questionBestCountAnsweredWrong) {
+                questionBestSoFar = question;
+                questionBestCountAnsweredWrong = countAnsweredWrong;
+            }
+        }
+
+        return questionBestSoFar;
+    }
+
+    @Nullable
+    private QuestionAndAnswer getQuestionAndAnswer(final String quizId, final String questionId) {
+        @NotNull final Quiz quiz = getQuiz(quizId);
+        return quiz.getQuestionAndAnswer(questionId);
     }
 
     /**
---
+++ src/main/java/com/murrayc/bigoquiz/shared/SubmissionResult.java
@@ -0,0 +1,51 @@
+package com.murrayc.bigoquiz.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Created by murrayc on 7/10/17.
+ */
+public class SubmissionResult implements IsSerializable {
+    private boolean result = false;
+    private Question.Text correctAnswer = null; //If result is false.
+    private Question nextQuestion = null;
+
+    public SubmissionResult() {
+    }
+
+    public SubmissionResult(final boolean result, final Question.Text correctAnswer, final Question nextQuestion) {
+        this.result = result;
+        this.correctAnswer = correctAnswer;
+        this.nextQuestion = nextQuestion;
+    }
+
+    public boolean getResult() {
+        return result;
+    }
+
+    /** This is just for the JSON input.
+     */
+    void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public Question.Text getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    /** This is just for the JSON input.
+     */
+    void setCorrectAnswer(final Question.Text correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public Question getNextQuestion() {
+        return nextQuestion;
+    }
+
+    /** This is just for the JSON input.
+     */
+    void setNextQuestion(final Question nextQuestion) {
+        this.nextQuestion = nextQuestion;
+    }
+}
