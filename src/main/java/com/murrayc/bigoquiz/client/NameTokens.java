989420fcccdbe76a91a4958d69ddd3bc5f7369ee
Murray Cumming <murrayc@murrayc.com>
2017-07-19 09:33:02 +0200

    page URLs: query parameters: Use hyphens instead of camel-case.

--- src/main/java/com/murrayc/bigoquiz/client/NameTokens.java
+++ src/main/java/com/murrayc/bigoquiz/client/NameTokens.java
@@ -8,9 +8,9 @@ public class NameTokens {
     public static final String QUIZ = "/quiz";
     public static final String QUIZ_LIST = "/quizzes";
     public static final String QUESTION = "/question";
-    public static final String PARAM_QUIZ_ID = "quizId";
-    public static final String QUESTION_PARAM_NEXT_QUESTION_SECTION_ID = "sectionId";
-    public static final String QUESTION_PARAM_QUESTION_ID = "questionId";
+    public static final String PARAM_QUIZ_ID = "quiz-id";
+    public static final String QUESTION_PARAM_NEXT_QUESTION_SECTION_ID = "section-id";
+    public static final String QUESTION_PARAM_QUESTION_ID = "question-id";
     public static final String USER_PROFILE = "/user";
     public static final String USER_HISTORY = "/history";
     public static final String ABOUT = "/about";
