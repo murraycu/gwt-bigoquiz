fd05e0b7ba45028843c3d16cb4646dc4412b01ce
Murray Cumming <murrayc@murrayc.com>
2017-10-12 21:49:04 +0200

    A tiny use of final.

--- src/main/java/com/murrayc/bigoquiz/server/api/CORSResponseFilter.java
+++ src/main/java/com/murrayc/bigoquiz/server/api/CORSResponseFilter.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 @Provider
 public class CORSResponseFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        final MultivaluedMap<String, Object> headers = responseContext.getHeaders();
         headers.add("Access-Control-Allow-Origin", "http://beta.bigoquiz.com");
         headers.add("Access-Control-Allow-Methods", "GET, POST");
 
